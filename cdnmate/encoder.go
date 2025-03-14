32db08a16c026bcfc5ef272cd908199bc2b26783
dedenfarhanhub <106763240+dedenfarhanhub@users.noreply.github.com>
2025-03-14 15:56:13 +0700

    cdn mate v.0.0.3

--- cdnmate/encoder.go
+++ cdnmate/encoder.go
@@ -3,29 +3,41 @@ package cdnmate
 import (
 	"bytes"
 	"fmt"
-	"github.com/chai2010/webp"
+	"github.com/kolesa-team/go-webp/webp"
 	"image"
 	"image/jpeg"
 	"image/png"
+
+	"github.com/kolesa-team/go-webp/encoder"
 )
 
-// ImageEncoder handles encoding and decoding images
+// ImageEncoder handles encoding images into different formats
 type ImageEncoder interface {
-	EncodeImage(buf *bytes.Buffer, img image.Image, format string, imageQuality float32) error
+	EncodeImage(buf *bytes.Buffer, img image.Image, format string, quality int) error
 }
 
 // DefaultEncoder implements ImageEncoder
 type DefaultEncoder struct{}
 
-// EncodeImage converts an image to a buffer in the given format
-func (s *DefaultEncoder) EncodeImage(buf *bytes.Buffer, img image.Image, format string, imageQuality float32) error {
+// EncodeImage converts an image to a buffer in the specified format with quality settings
+func (e *DefaultEncoder) EncodeImage(buf *bytes.Buffer, img image.Image, format string, quality int) error {
 	switch format {
 	case "jpeg", "jpg":
-		return jpeg.Encode(buf, img, &jpeg.Options{Quality: int(imageQuality)})
+		opts := jpeg.Options{Quality: quality}
+		return jpeg.Encode(buf, img, &opts)
 	case "png":
 		return png.Encode(buf, img)
 	case "webp":
-		return webp.Encode(buf, img, &webp.Options{Quality: imageQuality})
+		options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, float32(quality))
+		if err != nil {
+			return fmt.Errorf("failed to create WebP encoder options: %w", err)
+		}
+		// Encode image to WebP
+		if err = webp.Encode(buf, img, options); err != nil {
+			return fmt.Errorf("failed to encode WebP image: %w", err)
+		}
+
+		return nil
 	default:
 		return fmt.Errorf("unsupported format: %s", format)
 	}
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.23.0
 toolchain go1.23.6
 
 require (
-	github.com/chai2010/webp v1.1.1
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+	github.com/kolesa-team/go-webp v1.0.4
+	golang.org/x/image v0.25.0
 )
